1c5dca87d04a62f7d05208ffdcd90470ca7349c8
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-04 23:51:04 +0000

    build: bootstrap project

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,34 @@ repositories {
 }
 
 dependencies {
+    // spring boot
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+
+    // postgresql
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.h2database:h2:2.3.232")
 }
 
 tasks.withType<Test> {
