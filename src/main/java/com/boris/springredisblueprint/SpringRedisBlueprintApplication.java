4ed16f67cf479e5c7434bcfe6d99268936b00998
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-04 15:18:51 +0000

    First blood

---
+++ src/main/java/com/boris/springredisblueprint/SpringRedisBlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.boris.springredisblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisBlueprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisBlueprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boris/springredisblueprint/SpringRedisBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris.springredisblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisBlueprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
