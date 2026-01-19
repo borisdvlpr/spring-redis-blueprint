6572e3fc9e8d94a4475c5ca2a3cb4b42d478b4c9
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-19 23:17:03 +0000

    fix(repository): add @EntityGraph to findAll methods in the repository to prevent N+1 performance issues

--- src/main/java/com/boris/springredisblueprint/repository/PostRepository.java
+++ src/main/java/com/boris/springredisblueprint/repository/PostRepository.java
@@ -5,6 +5,7 @@ import com.boris.springredisblueprint.domain.entities.Category;
 import com.boris.springredisblueprint.domain.entities.Post;
 import com.boris.springredisblueprint.domain.entities.Tag;
 import com.boris.springredisblueprint.domain.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,13 +14,18 @@ import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    @EntityGraph(attributePaths = {"author", "category", "tags"})
     List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag);
 
+    @EntityGraph(attributePaths = {"author", "category", "tags"})
     List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
 
+    @EntityGraph(attributePaths = {"author", "category", "tags"})
     List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tag);
 
+    @EntityGraph(attributePaths = {"author", "category", "tags"})
     List<Post> findAllByStatus(PostStatus status);
 
+    @EntityGraph(attributePaths = {"author", "category", "tags"})
     List<Post> findAllByAuthorAndStatus(User author, PostStatus status);
 }
