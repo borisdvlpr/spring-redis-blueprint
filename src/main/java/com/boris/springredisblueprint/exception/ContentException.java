ca13ca5170a90d9bd2d276dcf1ef0f952082b14d
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-23 19:31:04 +0000

    feat(exception): create ContentException parent custom exception class

---
+++ src/main/java/com/boris/springredisblueprint/exception/ContentException.java
@@ -0,0 +1,22 @@
+package com.boris.springredisblueprint.exception;
+
+public class ContentException extends RuntimeException {
+    public ContentException() {
+    }
+
+    public ContentException(String message) {
+        super(message);
+    }
+
+    public ContentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ContentException(Throwable cause) {
+        super(cause);
+    }
+
+    public ContentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
