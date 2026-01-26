521d1ccf3e9877976460291bb8111dc32f87913b
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-26 21:56:33 +0000

    fix(domain): add field validation on LoginRequest

--- src/main/java/com/boris/springredisblueprint/model/dto/LoginRequest.java
+++ src/main/java/com/boris/springredisblueprint/model/dto/LoginRequest.java
@@ -1,15 +1,22 @@
 package com.boris.springredisblueprint.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+
 public class LoginRequest {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
     private String password;
 }
