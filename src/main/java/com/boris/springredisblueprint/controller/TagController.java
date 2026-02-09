d55b45314bd0b108e4fa0065a386971e5ee33e78
borisdvlpr <eduffmiranda@hotmail.com>
2026-02-09 19:04:47 +0000

    feat(controller): improve logging on tag controller

--- src/main/java/com/boris/springredisblueprint/controller/TagController.java
+++ src/main/java/com/boris/springredisblueprint/controller/TagController.java
@@ -1,12 +1,13 @@
 package com.boris.springredisblueprint.controller;
 
+import com.boris.springredisblueprint.mapper.TagMapper;
 import com.boris.springredisblueprint.model.dto.CreateTagsRequestDto;
 import com.boris.springredisblueprint.model.dto.TagDto;
 import com.boris.springredisblueprint.model.entity.Tag;
-import com.boris.springredisblueprint.mapper.TagMapper;
 import com.boris.springredisblueprint.service.command.TagCommandService;
 import com.boris.springredisblueprint.service.query.TagQueryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Log4j2
 @RestController
 @RequestMapping(path = "/api/v1/tags")
 @RequiredArgsConstructor
@@ -24,26 +26,33 @@ public class TagController {
 
     @GetMapping
     public ResponseEntity<List<TagDto>> getAllTags() {
+        log.info("GET /api/v1/tags");
         List<Tag> tags = tagQueryService.getTags();
         List<TagDto> tagRespons = tags.stream().map(tagMapper::toTagResponse).toList();
 
+        log.debug("Returning {} tags", tags.size());
         return ResponseEntity.ok(tagRespons);
     }
 
     @PostMapping
     public ResponseEntity<List<TagDto>> createTags(@RequestBody CreateTagsRequestDto createTagsRequestDto) {
+        log.info("POST /api/v1/tags");
         List<Tag> savedTags = tagCommandService.createTags(createTagsRequestDto.getNames());
-        List<TagDto> createdTagRespons = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        List<TagDto> createdTagsResponse = savedTags.stream().map(tagMapper::toTagResponse).toList();
 
+        log.debug("Created {} tags", createdTagsResponse.size());
         return new ResponseEntity<>(
-                createdTagRespons,
+                createdTagsResponse,
                 HttpStatus.CREATED
         );
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        log.info("DELETE /api/v1/tags/{}", id);
         tagCommandService.deleteTag(id);
+
+        log.debug("Deleted tag - id: '{}'", id);
         return ResponseEntity.noContent().build();
     }
 
