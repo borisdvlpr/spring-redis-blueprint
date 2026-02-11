8810995c2f1dfbc44ccda469e9e080409778bea3
borisdvlpr <eduffmiranda@hotmail.com>
2026-02-11 22:57:40 +0000

    fix(service): make tag repository private final in TagCommandServiceImpl

--- src/main/java/com/boris/springredisblueprint/service/command/impl/TagCommandServiceImpl.java
+++ src/main/java/com/boris/springredisblueprint/service/command/impl/TagCommandServiceImpl.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class TagCommandServiceImpl implements TagCommandService {
-    TagRepository tagRepository;
+    private final TagRepository tagRepository;
 
     @Override
     @Transactional
