da8ec30ca83db4b350cd0e174eed5bfde91cb652
borisdvlpr <eduffmiranda@hotmail.com>
2026-02-24 23:46:31 +0000

    feat(config): enable swagger paths in SecurityFilterChain configuration

--- src/main/java/com/boris/springredisblueprint/config/SecurityConfig.java
+++ src/main/java/com/boris/springredisblueprint/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
