74cc160e5450e0d67a30cf3c9d6d4e7ba379d7f8
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-19 23:14:38 +0000

    chore: update base test application execution

--- src/test/java/com/boris/springredisblueprint/SpringRedisBlueprintApplicationTests.java
+++ src/test/java/com/boris/springredisblueprint/SpringRedisBlueprintApplicationTests.java
@@ -3,11 +3,9 @@ package com.boris.springredisblueprint;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringRedisBlueprintApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
