6244885e5b2ba5f3f7ec3ee046673f717ec04381
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-21 19:17:31 +0000

    build: replace spring-boot testcontainers dependency with core and junit-jupiter modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers:2.0.4")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("com.h2database:h2:2.3.232")
 
