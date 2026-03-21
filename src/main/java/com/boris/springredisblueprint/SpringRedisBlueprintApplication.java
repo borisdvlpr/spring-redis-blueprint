6a0de7bfbac22eac49465784270f0ee0f9835877
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-21 21:59:08 +0000

    fix(config): move @EnableCaching to RedisConfig class

--- src/main/java/com/boris/springredisblueprint/SpringRedisBlueprintApplication.java
+++ src/main/java/com/boris/springredisblueprint/SpringRedisBlueprintApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class SpringRedisBlueprintApplication {
     static void main(String[] args) {
         SpringApplication.run(SpringRedisBlueprintApplication.class, args);
--- src/main/java/com/boris/springredisblueprint/config/RedisConfig.java
+++ src/main/java/com/boris/springredisblueprint/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.boris.springredisblueprint.config;
 
 import com.boris.springredisblueprint.model.dto.PostDto;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
